1a3c939dcabee807730e55776d31ebdd683a2c36
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-02-28 22:00:12 +0530

    Java Program to Display Current Date and Time

---
+++ datetime.java
@@ -0,0 +1,13 @@
+package DateTime.java;
+import java.util.*;
+public class datetime {
+	 public static void main(String args[])
+	    {
+	        Date current_Date = new Date();
+	        //"Date" class
+	        //"current_Date" is Date object
+	 
+	        System.out.println(current_Date);
+	        // print the time and date
+	    }
+}
