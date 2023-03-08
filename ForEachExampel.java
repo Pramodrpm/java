74e213dcc887ccb4bf1b51fcb270b028363a5a0d
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-08 19:42:02 +0530

    for loop in java

---
+++ ForEachExampel.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class ForEachExampel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String[] laptops={"lenovo","Acer","Delrerwwrl"};
+     for(String y: laptops)
+     {
+    	 System.out.println(y);
+     }
+	}
+
+}
