599780ec2f25921b797f69b37ffa5403a678a11c
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-08 19:46:53 +0530

    break statement in java

---
+++ Break.java
@@ -0,0 +1,19 @@
+
+public class Break {
+
+	public static void main(String[] args) {
+		
+	// TODO Auto-generated method stub
+    String s= "Pramod r";
+    for (int i=0;i<7;i++)
+    {
+    	if (s==" ")
+    	{
+    		break;
+    	}
+   System.out.println("your first name is "+); 
+   }
+    
+	}
+
+}
