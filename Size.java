5abc13a53f1b4a6da5cadc1598ea4ccccad9d16f
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-08 19:38:23 +0530

    java

---
+++ Size.java
@@ -0,0 +1,14 @@
+
+public class Size {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int a[]= {1,2,'a'};
+	int s=a.length;
+	System.out.println(s);
+	for(int i=0; i<s;i++)
+		System.out.println(a[i]);
+	
+	}
+
+}
---
+++ TwoDimension.java
@@ -0,0 +1,13 @@
+
+public class TwoDimension {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][]a= {{10,20,30,},{40,50,60}};
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println(a[0][1]);
+		}
+	}
+
+}
