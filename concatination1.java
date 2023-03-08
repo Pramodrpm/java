f67cb018d3aab5492500074fe8854ddf27dd695b
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-08 19:40:22 +0530

    java

---
+++ concatination1.java
@@ -0,0 +1,20 @@
+package concatination;
+
+public class concatination1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="pra";
+		String s2="mod";
+		
+		String s3
+		
+		
+		
+		
+		;
+		System.out.println(s3=s1+s2);
+		System.out.println(s3);
+	}
+
+}
