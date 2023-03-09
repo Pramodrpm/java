61398ad26c21e296b929970a9b995b06ebf783c5
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-09 22:19:46 +0530

    java argument constructor

---
+++ B.java
@@ -0,0 +1,25 @@
+package argumentconstructor;
+
+public class B {
+	A4 obj;
+    B(A4 obj){
+	  this.obj=obj;
+}
+    void display() {
+    	System.out.println(obj.data);
+    }
+}
+class A4{
+	int data=100;
+	A4(){
+		B b=new B(this);
+		b.display();
+	}
+	}
+	class Main{
+	public static void main(String[] args) {
+		A4 a=new A4();
+
+	}
+
+}
