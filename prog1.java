35360aed8c76066f1b7722f7c9ce7255b987a810
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-13 12:06:21 +0530

    java program

---
+++ prog1.java
@@ -0,0 +1,15 @@
+public class prog1 {
+    static void print(int a, int b) {
+        int c;
+
+        System.out.println(c = a + b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hii");
+        print(2, 3);
+        prog1 p = new prog1();
+        p.print(10, 20);
+    }
+
+}
