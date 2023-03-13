3bb36c46b0013d3a7cba4cd04475b1af496ea97b
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-13 23:34:39 +0530

    java program

---
+++ escape.java
@@ -0,0 +1,14 @@
+public class escape {
+
+    public static void main(String[] args) {
+        int x = 0, y = 0;
+        for (x = 0; x < 5; x++) {
+            for (y = 0; y < 5; y++) {
+                if (x > 1)
+                    break;
+            }
+
+            System.out.println("pramod");
+        }
+    }
+}
