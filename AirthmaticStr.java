f10a33597ab0a9fe607d3b9066ca3a7608fb4415
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-15 22:37:52 +0530

    java program to add two strings

---
+++ AirthmaticStr.java
@@ -0,0 +1,13 @@
+package airthmaticStrCat;
+
+public class AirthmaticStr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String st="COMPUTER";
+		String tg=" SCIENCE";
+		String res=st+tg;
+		System.out.println(res);	
+	}
+
+}
